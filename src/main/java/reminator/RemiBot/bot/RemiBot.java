9fd4e0629932ed84764fe72b5eb0a0e1192cf19d
Feavy <quentin071100@live.fr>
2020-10-30 18:36:34 +0100

    ça fix

--- src/main/java/reminator/RemiBot/bot/RemiBot.java
+++ src/main/java/reminator/RemiBot/bot/RemiBot.java
@@ -1,10 +1,11 @@
 package reminator.RemiBot.bot;
 
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class RemiBot {
 
@@ -13,7 +14,7 @@ public class RemiBot {
 
     public static void main(String[] arguments) throws Exception {
         token = arguments[0];
-        JDA api = new JDABuilder(AccountType.BOT).setToken(token).build();
+        JDA api = JDABuilder.create(token, GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES).enableCache(CacheFlag.ACTIVITY).build();
         api.addEventListener(new Controller());
         api.getPresence().setPresence(OnlineStatus.ONLINE, Activity.listening("r!help"));
     }
