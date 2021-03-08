4cdcfc0061a5754a08307ea1f54158cd91f8b57e
Feavy <quentin071100@live.fr>
2021-03-08 18:37:32 +0100

    Integration with webapp.

--- src/main/java/reminator/RemiBot/bot/RemiBot.java
+++ src/main/java/reminator/RemiBot/bot/RemiBot.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import reminator.RemiBot.Commands.nsfw.NSFWManager;
 
 public class RemiBot {
 
