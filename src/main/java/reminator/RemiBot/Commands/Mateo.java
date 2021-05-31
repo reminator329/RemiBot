3da3f15ccc11b09c9d73cf074f80eb25966d8ad1
Rémi <rlaborie2000@gmail.com>
2021-05-31 09:07:42 +0200

    test

--- src/main/java/reminator/RemiBot/Commands/Mateo.java
+++ src/main/java/reminator/RemiBot/Commands/Mateo.java
@@ -56,22 +56,23 @@ public class Mateo extends Command {
 
 
                     try {
-                        String p = new HTTPRequest("https://twitter.com/i/api/2/timeline/conversation/1392212382831816705.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_ext_alt_text=true&include_quote_count=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweet=true&count=20&include_ext_has_birdwatch_notes=false&ext=mediaStats%2ChighlightedLabel")
+                        String p = new HTTPRequest("https://twitter.com/i/api/graphql/L15nBTK_B0O_NMEpnsH4MQ/UserTweets?variables=%7B%22userId%22%3A%22611141533%22%2C%22count%22%3A20%2C%22withHighlightedLabel%22%3Atrue%2C%22withTweetQuoteCount%22%3Atrue%2C%22includePromotedContent%22%3Atrue%2C%22withTweetResult%22%3Afalse%2C%22withReactions%22%3Afalse%2C%22withUserResults%22%3Afalse%2C%22withVoice%22%3Afalse%2C%22withNonLegacyCard%22%3Atrue%2C%22withBirdwatchPivots%22%3Afalse%7D")
                                 .withHeader("accept-language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7")
                                 .withHeader("authorization", "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA")
-                                .withHeader("cookie", "_sl=1; personalization_id=\"v1_oRZugRsMECI++UDH3RMehQ==\"; guest_id=v1%3A162085596435924474; ct0=a4014bb283e5dcffdd9e1a5070026109; _twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCExKi2J5AToMY3NyZl9p%250AZCIlM2QwMWI3OWFiNmVmMmFhYmEwYmMxODk1OWU5YTczMTc6B2lkIiVlM2Nl%250AMzZiMjRhM2Q0ZGJmY2Q1M2IxZTdkNjVhZGViOA%253D%253D--678ba1feeb5cebc249025a654fe16fdddae3a3e2; gt=1392596973203693578")
-                                .withHeader("referer", "https://twitter.com/JulieOudet/status/1392212382831816705")
+                                .withHeader("cookie", "personalization_id=\"v1_oRZugRsMECI++UDH3RMehQ==\"; guest_id=v1%3A162085596435924474; ct0=e368e290cf71d6a224ee53f9f3e3cc1d; _sl=1; gt=1395068176887697413; _twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCFV6qYV5AToMY3NyZl9p%250AZCIlMmQxNjUxYjg2NTMzNjdlODk1ODQ4YTg5NTM4NTAwMGI6B2lkIiVlYzQz%250ANjYwZjU0ZTRkNDFiMmQ3ZTkwMzlkYjg0MTgzMg%253D%253D--58c2ea640e03f262e4068570c5242774c9fefded")
+                                .withHeader("referer", "https://twitter.com/JulieOudet")
                                 .withHeader("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"")
                                 .withHeader("sec-ch-ua-mobile", "?0")
                                 .withHeader("sec-fetch-dest", "empty")
                                 .withHeader("sec-fetch-mode", "cors")
                                 .withHeader("sec-fetch-site", "same-origin")
                                 .withHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
-                                .withHeader("x-csrf-token", "a4014bb283e5dcffdd9e1a5070026109")
-                                .withHeader("x-guest-token", "1392596973203693578")
+                                .withHeader("x-csrf-token", "e368e290cf71d6a224ee53f9f3e3cc1d")
+                                .withHeader("x-guest-token", "1395068176887697413")
                                 .withHeader("x-twitter-active-user", "yes")
                                 .withHeader("x-twitter-client-language", "fr")
                                 .GET();
+                        System.out.println(p);
 
                         JSONObject page = new JSONObject(p);
                         String conversation = page.getJSONObject("timeline").getString("id");
