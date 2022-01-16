168d35a23b584ee1e22946127d8841b7e551a33d
Rémi <rlaborie2000@gmail.com>
2022-01-16 18:58:32 +0100

    UPDATE : RepondeurService

--- src/main/java/reminator/RemiBot/Services/repondeur/RepondeurService.java
+++ src/main/java/reminator/RemiBot/Services/repondeur/RepondeurService.java
@@ -34,10 +34,17 @@ public class RepondeurService extends ListenerAdapter {
             Pattern patterndi = Pattern.compile("(?i)(di.*)");
             Matcher matcherdi = patterndi.matcher(arg);
 
+            Pattern patterndy = Pattern.compile("(?i)(dy.*)");
+            Matcher matcherdy = patterndy.matcher(arg);
+
             Pattern patterncri = Pattern.compile("(?i)(cri.*)");
             Matcher matchercri = patterncri.matcher(arg);
 
             if (matcherdi.find() && matcherdi.group(1).length() > 2) {
+                String message = matcherdi.group(1).substring(2);
+                EnvoiMessage.sendMessage(event, message.toLowerCase());
+            }
+            if (matcherdy.find() && matcherdy.group(1).length() > 2) {
                 String message = matcherdi.group(1).substring(2);
                 EnvoiMessage.sendMessage(event, message.toLowerCase());
             }
